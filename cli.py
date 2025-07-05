245a007ca11904b18b866e59915977a1c3ac9dba
Michael Little <michaellittle92@gmail.com>
2025-07-05 16:37:48 +1000

    cli ui for testing

---
+++ cli.py
@@ -0,0 +1,29 @@
+from main import log_in, register, get_all_users
+import sys
+
+menu_items = ["1. read all users", "2. register new user", "3. log in", "Q. quit"]
+
+def get_login_details():
+    username = input("username: ")
+    password = input("password: ")
+    return username, password
+
+while True:
+    for item in menu_items:
+        print(item)
+    user_input = input("Selection: ").capitalize().strip()
+    if user_input == "1":
+        get_all_users()
+    elif user_input == "Q":
+       sys.exit()
+    elif user_input == "2":
+        username, password = get_login_details()
+        register(username, password)
+        print("user registered")
+    elif user_input == "3":
+        username, password = get_login_details()
+        log_in(username, password)
+    else:
+        print("error, please try again.")
+
+
